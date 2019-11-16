7416dc2507c3ec275f92af901e02c043265b7275
Nir Lanka <nir@evildino.com>
2019-11-17 04:59:29 +0800

    Add dotnet core commands (unfinished)

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 rm -rf bin
 mkdir bin
 
-mcs one.cs -target:library -out:bin/one.dll
+cp /usr/local/share/dotnet/shared/Microsoft.NETCore.App/3.0.0/libhostpolicy.dylib bin/libhostpolicy.dylib
 
+csc one.cs -target:library -out:bin/one.dll
+# mcs one.cs -target:library -out:bin/one.dll
+
+csc app.cs /reference:bin/one.dll -out:bin/app.exe
 mcs app.cs /reference:bin/one.dll -out:bin/app.exe
 # mcs app.cs
 # mcs app.cs /reference:packages/Newtonsoft.Json.12.0.3/lib/netstandard2.0/Newtonsoft.Json.dll
 
-mono bin/app.exe
+dotnet -r:osx.10.14 bin/app.exe
+# mono bin/app.exe
